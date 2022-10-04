5a463428987e8216efa2205e5b829f48d95a3d33
jebaofila2001 <114081205+jebaofila2001@users.noreply.github.com>
2022-10-04 12:52:16 +0530

    Add files via upload

---
+++ assignment 2/A.THANGAPUSHPAM/code.py.py
@@ -0,0 +1,18 @@
+import random
+
+
+While (true):
+
+      temp=random.randint(10,99)
+      humid=random.randint(10,99)	
+      Print("current temperature:",temp)
+      Print("current humidity:",humid,"%")
+      temp_ref= Import 37
+      humid _ref=35
+
+
+if temp>temp_ref and humid<humid_ref:
+      Print("Alarm on")
+else:
+     Print("Alarm off")
+     break
---
+++ assignment 2/P.NANTHINI/code.py
@@ -0,0 +1,11 @@
+import random
+i=5
+While (True):
+        a=random.randint(1,99)
+        b=random. randint(1,99)
+        If(a>37 and b<50):
+         Print("High Temperature and ALARM IS ON") 
+        elif(a<35 and b<65):
+         Print ("Low Temperature and ALARM IS OFF") 
+         else:
+            break
